6a56ffefb85bfdf2bb7ff54525fb09487880668b
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-30 16:49:54 +0100

    Insert datas into database

--- index.js
+++ index.js
@@ -13,25 +13,53 @@ const sequelize = new Sequelize('sequelize-learn','root', null, {
 //     console.log("Erreur de connection à la BDD !")
 //   })
 
-const user = sequelize.define("user", {
-	username: {
-		type: Sequelize.DataTypes.STRING,
-    allowNull: false
+const User = sequelize.define(
+	"user",
+	{
+		username: {
+			type: Sequelize.DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: Sequelize.DataTypes.STRING,
+			allowNull: false,
+		},
+		age: {
+			type: Sequelize.DataTypes.INTEGER,
+			defaultValue: 21,
+		},
 	},
-	password: {
-		type: Sequelize.DataTypes.STRING,
-    allowNull: false
-	},
-	age: {
-		type: Sequelize.DataTypes.INTEGER,
-    defaultValue: 21
+	{
+		timestamps: false,
 	}
-})
+)
 
-user.sync()
-  .then((data) => {
-  console.log("Table et modèle synchronisés avec succès !")
-  })
-  .catch((err) => {
-    console.log("Erreur lors de la synchronisation !")
-  })
+// User.sync()
+//   .then((data) => {
+//   console.log("Table et modèle synchronisés avec succès !")
+//   })
+//   .catch((err) => {
+//     console.log("Erreur lors de la synchronisation !")
+//   })
+
+User.sync({ alter: true })
+	.then(() => {
+		// const user = User.build({
+		// 	username: "Jonathan",
+		// 	password: "1234",
+		// 	age: 46,
+		// })
+		// return user.save()
+		return User.create({
+			username: "Jonathan",
+			password: "1234",
+			age: 46,
+		})
+	})
+	.then((data) => {
+		console.log(data.toJSON())
+		console.log("Utilisateur ajouté à la bdd !")
+	})
+	.catch((err) => {
+		console.log(err)
+	})
