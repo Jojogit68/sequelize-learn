8bb13e8a6f54ef84117ea01db2d530b745fedb7f
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-30 15:42:15 +0100

    Create model

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const sequelize = new Sequelize('sequelize-learn','root', null, {
   dialect: "mysql"
 })
 
-sequelize.authenticate()
-  .then(() => {
-    console.log("Connection établie à la BDD")
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log("Connection établie à la BDD")
+//   })
+//   .catch((err) => {
+//     console.log("Erreur de connection à la BDD !")
+//   })
+
+const user = sequelize.define("user", {
+	username: {
+		type: Sequelize.DataTypes.STRING,
+    allowNull: false
+	},
+	password: {
+		type: Sequelize.DataTypes.STRING,
+    allowNull: false
+	},
+	age: {
+		type: Sequelize.DataTypes.INTEGER,
+    defaultValue: 21
+	}
+})
+
+user.sync()
+  .then((data) => {
+  console.log("Table et modèle synchronisés avec succès !")
   })
   .catch((err) => {
-    console.log("Erreur de connection à la BDD !")
+    console.log("Erreur lors de la synchronisation !")
   })
